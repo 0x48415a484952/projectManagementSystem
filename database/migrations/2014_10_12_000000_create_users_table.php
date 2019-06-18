dda706cdf47d166630bef91094fc9b4d51aa0563
Hashem Mo <hashemm364@gmail.com>
2019-06-18 13:55:39 +0430

    Add ide-helper and create Models directory

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mobile')->unique();
+            //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar')->nullable();
+            $table->text('address')->nullable();
+            $table->text('description')->nullable();
+            //$table->rememberToken();
             $table->timestamps();
         });
     }
